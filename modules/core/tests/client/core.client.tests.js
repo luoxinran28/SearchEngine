1a44b75d01829e5ea13a9634a2633bb460151cc3
lumindon <lumindontech@gmail.com>
2016-05-14 14:25:55 -0600

    Add a wrong unit test

--- modules/core/tests/client/core.client.tests.js
+++ modules/core/tests/client/core.client.tests.js
@@ -4,4 +4,9 @@
   beforeAll(function () {
     angular.element(document.querySelector('head')).append('<base href="/">');
   });
+
+  it('should be wrong', function(done) {
+	  false.should.be.true();
+	  return done();
+	});
 }());
